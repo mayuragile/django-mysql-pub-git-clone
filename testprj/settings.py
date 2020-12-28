035d5100f665197c968224d4b51d551e3b0ea149
mayuragile <76421871+mayuragile@users.noreply.github.com>
2020-12-28 17:24:45 +0530

    Update settings.py

--- testprj/settings.py
+++ testprj/settings.py
@@ -69,28 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'testprj.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mysql',
-#         'USER': 'mysql',
-#         'PASSWORD': 'mysql',
-#         'HOST': 'db',
-#         'PORT': 3306,
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
