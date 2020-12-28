e3f7c394ffc028ba0b7e03b919a7404573684e62
mayuragile <mayur.barbhaya@agileinfoways.com>
2020-12-28 22:04:01 +0530

    'adsadsasdsd'

--- testprj/settings.py
+++ testprj/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
             # Tell MySQLdb to connect with 'utf8mb4' character set
             'charset': 'utf8mb4',
         },
-#         # Tell Django to build the test database with the 'utf8mb4' character set
+#         # Tell Django to build the (mysql named db) database with the 'utf8mb4' character set
         'mysql': {
             'CHARSET': 'utf8mb4',
             'COLLATION': 'utf8mb4_unicode_ci',
