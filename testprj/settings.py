d350375bf3606977bb3d231a507c3310d1d19fd6
mayuragile <76421871+mayuragile@users.noreply.github.com>
2020-12-28 23:08:07 +0530

    Update settings.py

--- testprj/settings.py
+++ testprj/settings.py
@@ -72,9 +72,9 @@ WSGI_APPLICATION = 'testprj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysql',
-        'USER': 'mysql',
-        'PASSWORD': 'mysql',
+        'NAME': 'test',
+        'USER': 'testuser',
+        'PASSWORD': 'testpassword',
         'HOST': 'db',
         'PORT': 3306,
         'OPTIONS': {
@@ -82,7 +82,7 @@ DATABASES = {
             'charset': 'utf8mb4',
         },
 #         # Tell Django to build the (mysql named db) database with the 'utf8mb4' character set
-        'mysql': {
+        'test': {
             'CHARSET': 'utf8mb4',
             'COLLATION': 'utf8mb4_unicode_ci',
         }
