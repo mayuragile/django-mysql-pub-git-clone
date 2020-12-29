ac37d9f25588e7ebf1c0d5f53ad17478335aae51
mayuragile <76421871+mayuragile@users.noreply.github.com>
2020-12-29 18:20:08 +0530

    Update settings.py

--- testprj/settings.py
+++ testprj/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'testprj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'my-app-db1',
+        'NAME': 'my-app-db3',
         'USER': 'root',
         'PASSWORD': 'password',
         'HOST': 'db',
